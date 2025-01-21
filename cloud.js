e7b1ddf8e4f1ada81ba14c28ad5efe4a38892270
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-01-21 20:44:21 +0200

    Optimize cloud.js

--- cloud.js
+++ cloud.js
@@ -1,37 +1,33 @@
 const paragraph = document.getElementById("myParagraph");
 const paragraphText = paragraph.textContent
   .toLowerCase()
-  .replaceAll(/,|\.|;|:|\\n/g, "");
+  .replaceAll(/[,\.;:\\n]/g, "");
 //   .replaceAll(",", "")
 //   .replaceAll(".", "")
 //   .replaceAll(";", "")
 //   .replaceAll(":", "")
 //   .replaceAll("\n", "");
-const paragraphWords = paragraphText.split(" ").filter((n) => {
-  // filter empty strings
-  return n !== "";
-});
-const wordCounts = paragraphWords.reduce((a, b) => {
-  a[b] = (a[b] || 0) + 1;
+
+// filter empty strings
+const paragraphWords = paragraphText.split(" ").filter((n) => n !== "");
+const wordsOccurenceCounts = paragraphWords.reduce((a, b) => {
+  a[b] = (a[b] ?? 0) + 1;
   return a;
 }, {});
 
-const wordEntries = Object.entries(wordCounts)
+const wordEntries = Object.entries(wordsOccurenceCounts)
   .sort((a, b) => {
     return b[1] - a[1];
   })
   .slice(0, 12);
-console.log(wordEntries);
+// console.log(wordEntries);
 
 const highestTwelve = Object.keys(Object.fromEntries(wordEntries));
 
 const container = document.getElementById("myWordCloud");
 // clear and style the container
 container.textContent = "";
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.gap = "4rem";
-container.style.margin = "4rem";
+container.classList.add("container");
 // create fragment
 const fragment = document.createDocumentFragment();
 // create a colors collection
@@ -54,11 +50,11 @@ highestTwelve.forEach((word, index) => {
   pElement.style.fontSize = `${fontSize}px`;
 
   // create a random rotation in 4 orientations
-  const rotation = Math.trunc(Math.random() * 4) * 90;
+  const rotation = Math.trunc(Math.random() * 5) * 90;
   pElement.style.rotate = `${rotation}deg`;
 
   // get a random color from the collection
-  const randomColor = Math.trunc(Math.random() * colorsCollection.length - 1);
+  const randomColor = Math.trunc(Math.random() * colorsCollection.length);
   pElement.style.color = colorsCollection[randomColor];
 
   // add the word as text content
